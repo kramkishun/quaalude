1bb19d89caffe2cd0daa8502bdc352da73b4482c
Kevin Ramkishun <kramkishun@protonmail.com>
2018-11-19 22:12:15 -0500

    Updated to work with the python backend which grabs the data from MongoDB and will in the future perform numerical work.

--- quaalude.js
+++ quaalude.js
@@ -1,6 +1,12 @@
+let request = require('request')
+
 const express = require('express');
 const app = express();
 const port = 3001;
+
+
+// launch with:
+// > node quaalude.js
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -8,11 +14,29 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Check out /tsdata'));
 
-app.get('/tsdata', (req, res) => {
-  let randomData = Array(40).fill().map(() => Math.round(Math.random() * 40));
-  res.json(randomData);
+// Right now just serving as a pass through
+app.get('/tsdata/:sym', (req, res) => {
+
+  const pickleRisk = 'http://localhost:5000/history/' + req.params.sym;
+
+  request(pickleRisk, (getErr, getRes, getBody) => {
+    if (getErr) {
+      console.log (getErr);
+      console.log ("Could not retrieve data from pickleRisk for: " + req.param.sym);
+      return;
+    }
+
+    console.log ("Received data from pickleRisk for " + req.param.sym);
+
+    datesAndCloses = JSON.parse(getBody);
+    res.json(datesAndCloses);
+  });
+
+  // return array of 40 random numbers
+  //let randomData = Array(40).fill().map(() => Math.round(Math.random() * 40));
+  //res.json(randomData);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Quaalude launched on port ${port}!`));
