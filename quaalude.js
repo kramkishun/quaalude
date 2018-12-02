1990e42d47133f0ab8ac0064b7f908069ccf3936
joe <jduplessis86@gmail.com>
2018-12-02 12:06:34 -0500

    Started adding file structure for logon

--- quaalude.js
+++ quaalude.js
@@ -2,8 +2,28 @@ let request = require('request')
 
 const express = require('express');
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
 
+var morgan       = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+
+
+var configDB = require('./config/database.js');
+
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+// require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
 
 // launch with:
 // > node quaalude.js
@@ -13,6 +33,19 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+// required for passport
+app.use(session({ secret: 'MethaqualoneMandraxQuinazolinone' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// routes ======================================================================
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+// launch ======================================================================
+app.listen(port);
+console.log('Quaalude launched on port' + port);
 
 app.get('/', (req, res) => res.send('Check out /tsdata'));
 
